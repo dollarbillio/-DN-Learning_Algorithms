c682d1513515b0efbaa2d884fdf1914ca11440ea
Vrishchik Rashi <32869837+dollarbillio@users.noreply.github.com>
2018-10-27 01:56:13 +0700

    Create BinarySearch_Recursion.py

---
+++ Search/BinarySearch_Recursion.py
@@ -0,0 +1,16 @@
+
+```python
+def binarysearch(list_1, result):
+    mid = len(list_1) // 2
+
+    if len(list_1) == 1:
+        if list_1[mid] == result:
+            return "There is Found"
+        else:
+            return "There is None" 
+    else: 
+        if list_1[mid] > result:
+            return binarysearch(list_1[:mid], result)
+        elif list_1[mid] <= result:
+            return binarysearch(list_1[mid:], result)
+```
