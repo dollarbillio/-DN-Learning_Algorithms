f82834a7b70f45f15a61aefb591583afdd80fad3
Vrishchik Rashi <32869837+dollarbillio@users.noreply.github.com>
2018-10-28 17:01:22 +0700

    Update and rename main.py to ForexTradeCombo.py

--- BitWise/ForexTradeCombo.py
+++ BitWise/ForexTradeCombo.py
@@ -1,5 +1,7 @@
 
-
+---------------------------------------------
+# Create all trade_combo by BitWise Algorithm
+---------------------------------------------
 def trade_combo(list_1):
 	'''Return a combo of possible buy and sell between currency
 	   the trade combo return an iterable by using yield
@@ -23,27 +25,35 @@ all_combo = []
 for i in trade_combo(['EUR', 'USD', 'JPY', 'GBP', 'AUD', 'CAD']):
 	all_combo.append(i)
 
+------------------------------------------------
+# Modify so that currency possibility is encoded
+------------------------------------------------
 modified = []
 for i in all_combo:
 	dict_sign = {}
 	for j in i:
 		dict_sign[j[0]] = dict_sign.get(j[0], 0) + 1
 	
-	'''Find the market leading for currency'''
+	# Find the market leading currency
 	min_value = min(list(dict_sign.values()))
-	if min_value == 1 or min_value == 2:
+    
+    # Find the currencies with stable sign 
+	if min_value in [1,2]:
 		for keys, values in dict_sign.items():
-			if values == 1 or values == 2:
+			if values in [1,2]:
 				modified.append([a for a in i if keys in a])
-	else:
+	# If there are no clear currency direction
+    else:
 		modified.append([0])
 
 print ("Here is our trade")
 
-print (len(all_combo) == len(modified))
-
+-------------------------------
+# Most Possible Currency Trades
+-------------------------------
 for i in range (len(modified)):
-	print (str (all_combo[i]) + ' >>> ' + str(modified[i])) 
+    if modified[i] != [0]:
+	    print (str (all_combo[i]) + ' >>> ' + str(modified[i])) 
 	
 			
 
