8276f3c5b41d063816ea8d6f6daca1ac5145c9cc
Vrishchik Rashi <32869837+dollarbillio@users.noreply.github.com>
2018-10-29 17:06:14 +0700

    Update ForexTradeCombo.py

--- BitWise/ForexTradeCombo.py
+++ BitWise/ForexTradeCombo.py
@@ -8,11 +8,14 @@ def trade_combo(list_1):
     N = len(list_1)
     currency_dict = {}
     for i in range(N):
+        # Associate a number index to a currency
         currency_dict[i] = list_1[i] 
 	
     for i in range(2**N):
         Combo = []
         for j in range(N):
+	        # Each index of number corresponds to a currency position
+            # Shift
             if (i // 2**j) % 2 == 1:
                 Combo.append("- " + currency_dict[j])
             else:
@@ -30,12 +33,13 @@ for i in trade_combo(['EUR', 'USD', 'JPY', 'GBP', 'AUD', 'CAD']):
 modified = []
 for i in all_combo:
     dict_sign = {}
+    '''Get the maximum sign in currency combo situation'''
     for j in i:
         dict_sign[j[0]] = dict_sign.get(j[0], 0) + 1
 	
     # Find the market leading currency
     min_value = min(list(dict_sign.values()))
-    # Find the currencies with stable sign 
+    # Find the currencies with leading power 
     if min_value in [1,2]:
         for keys, values in dict_sign.items():
             if values in [1,2]:
