bbdaf5e17263d688234158802757fb374d82100c
Vrishchik Rashi <32869837+dollarbillio@users.noreply.github.com>
2018-11-19 11:20:22 +0700

    Create SetFunction.py

---
+++ Python/SetFunction.py
@@ -0,0 +1,11 @@
+fruits = set(["avocado", "tomato", "banana"])
+vegi = set(["beets", "carrots", "tomato"])
+
+// intersections
+print (fruits | vegi)
+
+// Union, share all items, remove duplicated values
+print (fruits & vegi)
+
+// Show things that fruits has but vegi doesn'help
+print (fruits - vegi)
